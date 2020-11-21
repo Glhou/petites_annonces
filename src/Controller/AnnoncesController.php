357a7450b87d8fbba7c1e7c26640df5bda5940d2
Glenn <g.louedec@live.fr>
2020-11-21 17:47:22 +0100

    Affichage vite fait des annonces dans /annonces.

--- src/Controller/AnnoncesController.php
+++ src/Controller/AnnoncesController.php
@@ -5,15 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Ad;
+use App\Entity\Comment;
+use App\Entity\User;
+use App\Repository\AdRepository;
+use App\Repository\CommentRepository;
+use App\Repository\UserRepository;
+
 
 class AnnoncesController extends AbstractController
 {
     /**
      * @Route("/annonces", name="annonces")
      */
-    public function index(): Response
+    public function index(AdRepository $repo): Response
     {
+        $ad = $repo->findAll();
         return $this->render('annonces/index.html.twig', [
+            'Ad' => $ad,
         ]);
     }
 
