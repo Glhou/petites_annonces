90f7bc74fa93f77dce74d3dc0b1698c6a7ebcb78
Glenn <g.louedec@live.fr>
2020-12-09 18:22:07 +0100

    Modification des fixtures (pas de ficher doublon, email, user dans comment des autres, password ,...)

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -13,41 +13,43 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = \Faker\Factory::create("fr_FR");
-
-        for ($i = 1 ; $i <= 10; $i++){
+        $user_list = [] ;
+        for ($i = 1 ; $i <= 100; $i++){
             $user = new User();
-            $user->setUsername($faker->name)
-                 ->setEmail($faker->email)
-                 ->setPassword($faker->word)
+            $nom = $faker->userName;
+            $user->setUsername($nom)
+                 ->setEmail($nom."@".$faker->freeEmailDomain)
+                 ->setPassword("test")
                  ->setFirstName($faker->firstName)
                  ->setRoles($faker->creditCardDetails)
                  ->setLastName($faker->lastName)
                  ->setPromo($faker->numberBetween($min = 2010, $max = 2023));
+            $user_list[$i] = $user;
             $manager->persist($user);
+        }
+        for ($j = 1 ; $j <= mt_rand(150,250);$j++){
+            $ad = new Ad();
+            $dateAd = $faker->dateTimeBetween('-6 months');
+            $ad->setAuthor($user_list[mt_rand(1,100)])
+                ->setTitle($faker->sentence($nbWords = 3, $variableNbWords = true))
+                ->setResolved($faker->boolean)
+                ->setDescription($faker->text($maxNbChars = 200))
+                ->setLocation($faker->city)
+                ->setDate($dateAd)
+                ->setType($faker->numberBetween($min = 0, $max = 5));
+            $manager -> persist($ad);
 
-            for ($j = 1 ; $j <= mt_rand(3,5);$j++){
-                $ad = new Ad();
-                $dateAd = $faker->dateTimeBetween('-6 months');
-                $ad->setAuthor($user)
-                    ->setTitle($faker->sentence($nbWords = 3, $variableNbWords = true))
-                    ->setResolved($faker->boolean)
-                    ->setDescription($faker->text($maxNbChars = 200))
-                    ->setLocation($faker->city)
-                    ->setDate($dateAd)
-                    ->setType($faker->numberBetween($min = 0, $max = 5));
-                $manager -> persist($ad);
-
-                for ($l = 1; $l <= mt_rand(6,10); $l++){
-                    $comment = new Comment();
-                    $days = (new \DateTime())->diff($ad->getDate())->days;
-                    $comment->setAuthor($user)
-                        ->setAd($ad)
-                        ->setDate($faker->dateTimeBetween('-' . $days . 'days'))
-                        ->setContent($faker->text($maxNbChars = 200));
-                    $manager->persist($comment);
-                }
+            for ($l = 1; $l <= mt_rand(60,100); $l++){
+                $comment = new Comment();
+                $days = (new \DateTime())->diff($ad->getDate())->days;
+                $comment->setAuthor($user_list[mt_rand(1,100)])
+                    ->setAd($ad)
+                    ->setDate($faker->dateTimeBetween('-' . $days . 'days'))
+                    ->setContent($faker->text($maxNbChars = 200));
+                $manager->persist($comment);
             }
         }
+        
 
         $manager->flush();
     }
