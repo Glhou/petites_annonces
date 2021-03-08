86fc0e2b8d0f3b8c926ff71fed326b5ed7b4a5d7
Glenn <g.louedec@live.fr>
2021-03-08 18:02:33 +0100

    Possibilité de récupèrer tout les users et d'en récupèrer un par son id

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,32 +19,18 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
+    public function allUsers(){
+        return $this->createQueryBuilder("a")
+            ->orderBy("a.username")
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
     }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
+    public function getUserById(Int $id){
+        return $this->createQueryBuilder("a")
+            ->where("a.id =:id")
+            ->setParameter("id",$id)
             ->getQuery()
-            ->getOneOrNullResult()
-        ;
+            ->getResult();
     }
-    */
 }
